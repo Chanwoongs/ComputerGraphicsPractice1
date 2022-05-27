879e9258116ae154634dbaa7bed5f8586fdf92ad
Chanwoongs <tkfkdcksdnd@daum.net>
2022-05-27 17:55:03 +0900

    First Person Camera 기능 추가

--- ProjectAssignment3/cameraclass.cpp
+++ ProjectAssignment3/cameraclass.cpp
@@ -13,6 +13,21 @@ CameraClass::CameraClass()
 	m_rotation.x = 0.0f;
 	m_rotation.y = 0.0f;
 	m_rotation.z = 0.0f;
+
+	m_defaultRight = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+	m_defaultForward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	m_camRight = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+	m_camForward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+
+	m_up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	m_moveLeftRight = 0.0f;
+	m_moveBackForward = 0.0f;
+	m_moveUpDown = 0.0f;
+
+	m_yaw = 0.0f;
+	m_pitch = 0.0f;
+	m_roll = 0.0f;
 }
 
 
@@ -41,6 +56,36 @@ void CameraClass::SetRotation(float x, float y, float z)
 	m_rotation.z = z;
 }
 
+void CameraClass::MoveLeft(float speed)
+{
+	m_moveLeftRight -= speed;
+}
+
+void CameraClass::MoveRight(float speed)
+{
+	m_moveLeftRight += speed;
+}
+
+void CameraClass::MoveBack(float speed)
+{
+	m_moveBackForward -= speed;
+}
+
+void CameraClass::MoveForward(float speed)
+{
+	m_moveBackForward += speed;
+}
+
+void CameraClass::MoveDown(float speed)
+{
+	m_moveUpDown -= speed;
+}
+
+void CameraClass::MoveUp(float speed)
+{
+	m_moveUpDown += speed;
+}
+
 
 XMFLOAT3 CameraClass::GetPosition()
 {
@@ -56,38 +101,52 @@ XMFLOAT3 CameraClass::GetRotation()
 // This uses the position and rotation of the camera to build and to update the view matrix.
 void CameraClass::Render()
 {
-	XMVECTOR up, position, lookAt;
-	float yaw, pitch, roll;
-	XMMATRIX rotationMatrix;
+	XMVECTOR position;
 
-	// Setup the vector that points upwards.
-	up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	XMMATRIX rotationtempMatrix;
 
 	// Setup the position of the camera in the world.
 	position = XMLoadFloat3(&m_position);
 
-	// Setup where the camera is looking by default.
-	lookAt = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
-
 	// Set the yaw (Y axis), pitch (X axis), and roll (Z axis) rotations in radians.
-	pitch = m_rotation.x * 0.0174532925f;
-	yaw   = m_rotation.y * 0.0174532925f;
-	roll  = m_rotation.z * 0.0174532925f;
+	m_pitch = m_rotation.x * 0.0174532925f;
+	m_yaw = m_rotation.y * 0.0174532925f;
+	m_roll = m_rotation.z * 0.0174532925f;
+
+	camRotationMatrix = XMMatrixRotationRollPitchYaw(m_pitch, m_yaw, m_roll);
+	m_target = XMVector3TransformCoord(m_defaultForward, camRotationMatrix);
+	m_target = XMVector3Normalize(m_target);
 
 	// Create the rotation matrix from the yaw, pitch, and roll values.
-	rotationMatrix = XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
+	rotationtempMatrix = XMMatrixRotationY(m_yaw);
 
-	// Transform the lookAt and up vector by the rotation matrix so the view is correctly rotated at the origin.
-	lookAt = XMVector3TransformCoord(lookAt, rotationMatrix);
-	up = XMVector3TransformCoord(up, rotationMatrix);
+	m_camRight = XMVector3TransformCoord(m_defaultRight, rotationtempMatrix); // 회전한 만큼 카메라의 오른쪽 벡터 설정
+	m_up = XMVector3TransformCoord(m_up, rotationtempMatrix); // 회전한 만큼 카메라의 위쪽 벡터 설정
+	m_camForward = XMVector3TransformCoord(m_defaultForward, rotationtempMatrix); // 회전한 만큼 카메라의 앞 벡터 설정
+
+	position += m_moveLeftRight * m_camRight; // 좌우 위치 이동
+	position += m_moveBackForward * m_camForward; // 앞뒤 위치 이동
+	position += m_moveUpDown * m_up; // 앞뒤 위치 이동
+	m_position.x = XMVectorGetX(position);
+	m_position.y = XMVectorGetY(position);
+	m_position.z = XMVectorGetZ(position);
+
+	m_moveLeftRight = 0.0f; // 초기화
+	m_moveBackForward = 0.0f; // 초기화
+	m_moveUpDown = 0.0f;
 
 	// Translate the rotated camera position to the location of the viewer.
-	lookAt = position + lookAt;
+	m_target = position + m_target;
 
 	// Finally create the view matrix from the three updated vectors.
-	m_viewMatrix = XMMatrixLookAtLH(position, lookAt, up);
+	m_viewMatrix = XMMatrixLookAtLH(position, m_target, m_up);
 
 	return;
+}
+
+void CameraClass::Move(float speed)
+{
+
 }
 
 
@@ -95,3 +154,11 @@ void CameraClass::GetViewMatrix(XMMATRIX& viewMatrix)
 {
 	viewMatrix = m_viewMatrix;
 }
+
+void CameraClass::RotateCamera(float x, float y, float z)
+{
+	m_rotation.x += x;
+	m_rotation.y += y;
+	m_rotation.z += z;
+}
+
--- ProjectAssignment3/cameraclass.h
+++ ProjectAssignment3/cameraclass.h
@@ -26,17 +26,35 @@ public:
 
 	void SetPosition(float, float, float);
 	void SetRotation(float, float, float);
+	void MoveLeft(float);
+	void MoveRight(float);
+	void MoveBack(float);
+	void MoveForward(float);
+	void MoveDown(float);
+	void MoveUp(float);
 
 	XMFLOAT3 GetPosition();
 	XMFLOAT3 GetRotation();
 
 	void Render();
 	void GetViewMatrix(XMMATRIX&);
+	void RotateCamera(float, float, float);
+	void Move(float);
 
 private:
 	XMFLOAT3 m_position;
 	XMFLOAT3 m_rotation;
 	XMMATRIX m_viewMatrix;
+	XMMATRIX camRotationMatrix;
+
+	float m_moveLeftRight;
+	float m_moveBackForward;
+	float m_moveUpDown;
+
+	XMVECTOR m_up, m_target;
+	XMVECTOR m_camRight, m_camForward;
+	XMVECTOR m_defaultRight, m_defaultForward;
+	float m_yaw, m_pitch, m_roll;
 };
 
 #endif
